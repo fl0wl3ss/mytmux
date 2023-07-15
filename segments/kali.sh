ab7c545e605348f3b23e850cf5e45097bc68610a
root <root@LAPTOP-AJ7N12JV>
2023-07-15 21:11:05 +0900

    [init]

---
+++ segments/kali.sh
@@ -0,0 +1,7 @@
+# Prints the local network IPv4 address for a statically defined NIC or search for an IPv4 address on all active NICs.
+
+run_segment() {
+        echo " "
+		echo ""
+        return 0
+}
---
+++ segments/pad.sh
@@ -0,0 +1,6 @@
+# Prints the local network IPv4 address for a statically defined NIC or search for an IPv4 address on all active NICs.
+
+run_segment() {
+	echo "aa"
+	return 0
+}
